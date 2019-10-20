605ac0917685331ead1da89ff19a2376c07dad5e
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-20 11:38:01 +0530

    Update If-else.java

--- If-else.java
+++ If-else.java
@@ -9,11 +9,9 @@ import java.util.regex.*;
 public class Solution {
 
         private static final Scanner scanner = new Scanner(System.in);
-
         public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         scanner.close();
             if(N>1 && N<101 )
             {
@@ -27,11 +25,7 @@ public class Solution {
                     System.out.println("Not Weird");}
                     else if(N>5 && N<21) {                                                                    System.out.println("Weird");}
                     else if(N>20)  {                                                                          System.out.println("Not Weird");}
-
-
                 }
-
             }
         }
-    
-}
+ }
