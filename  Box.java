d72674ecabdccfee6aa31b6a1fef3fc8deb17b66
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-22 22:48:21 +0530

    programme to print length of box.

---
+++  Box.java
@@ -0,0 +1,14 @@
+public class Box
+{
+	private int l,b,h;
+	public Box()
+	{
+	l=10;b=8;h=4;
+	}
+
+ public static void main(String [] args)
+ {
+	Box b1 = new Box():
+	Sytem.out.println("length is"+l);
+ }
+}
