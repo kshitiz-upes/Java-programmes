d9768cfb97b424136f77b72dd37e726fc7a12bac
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-23 21:44:59 +0530

    Update Hello_world.java

--- Hello_world.java
+++ Hello_world.java
@@ -1,7 +1,8 @@
- class Hello_world {
-
-    public static void main(String args[]){
+class Hello_world
+{
+    public static void main(String args[])
+    {
      System.out.println("Hello World");
- }
+    }
 }
 
